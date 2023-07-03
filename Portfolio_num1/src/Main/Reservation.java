823315fa935b34baf1c79a8c7f9be3209cd9ec80
Class01 <Class01@DESKTOP-ILL1MFM>
2023-07-03 14:46:35 +0900

    Reservation

--- Portfolio_num1/src/Main/Reservation.java
+++ Portfolio_num1/src/Main/Reservation.java
@@ -9,9 +9,9 @@ import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
 import net.sourceforge.jdatepicker.impl.UtilDateModel;
 
 public class Reservation extends WindowAdapter implements ActionListener {
-	private JFrame Rsn;
-	private JLabel RId, RDate, RTime, RPeople, Rmemo, PM, MM;
-	private JTextField RtId, RtPe, Rtme;
+	private JFrame rsn;
+	private JLabel lId, lDate, lTime, lPeople, lmemo, lPM, lMM;
+	private JTextField fId, fPe, fme;
 	private Choice time;
 	private JButton res;
 	private String ID;
@@ -27,35 +27,35 @@ public class Reservation extends WindowAdapter implements ActionListener {
 
 	public Reservation(String ID) {
 		this.ID = ID;
-		Rsn = new JFrame("예약하기");
+		rsn = new JFrame("예약하기");
 
-		Rsn.setSize(450, 550);
-		Rsn.setLayout(null);
-		Rsn.setResizable(false);
-		Rsn.setLocationRelativeTo(null);
-		Rsn.getContentPane().setBackground(Color.white);
+		rsn.setSize(450, 550);
+		rsn.setLayout(null);
+		rsn.setResizable(false);
+		rsn.setLocationRelativeTo(null);
+		rsn.getContentPane().setBackground(Color.white);
 
-		RId = new JLabel("아이디");
-		RId.setBounds(53, 38, 72, 29);
-		RId.setFont(new Font("맑은 고딕", 0, 24));
-		RtId = new JTextField(ID);
-		RtId.setBounds(149, 34, 214, 38);
-		RtId.setFont(new Font("맑은 고딕", 0, 24));
-		RtId.setEditable(false);
-		RtId.setBackground(Color.white);
+		lId = new JLabel("아이디");
+		lId.setBounds(53, 38, 72, 29);
+		lId.setFont(new Font("맑은 고딕", 0, 24));
+		fId = new JTextField(ID);
+		fId.setBounds(149, 34, 214, 38);
+		fId.setFont(new Font("맑은 고딕", 0, 24));
+		fId.setEditable(false);
+		fId.setBackground(Color.white);
 
-		RDate = new JLabel("날짜");
-		RDate.setBounds(63, 103, 50, 29);
-		RDate.setFont(new Font("맑은 고딕", 0, 24));
+		lDate = new JLabel("날짜");
+		lDate.setBounds(63, 103, 50, 29);
+		lDate.setFont(new Font("맑은 고딕", 0, 24));
 
 		datePicker.setBounds(149, 98, 244, 38);
 		datePicker.setVisible(true);
 		datePicker.setFont(new Font("맑은 고딕", 0, 24));
 		datePicker.setBackground(Color.white);
 
-		RTime = new JLabel("시간");
-		RTime.setBounds(63, 168, 50, 29);
-		RTime.setFont(new Font("맑은 고딕", 0, 24));
+		lTime = new JLabel("시간");
+		lTime.setBounds(63, 168, 50, 29);
+		lTime.setFont(new Font("맑은 고딕", 0, 24));
 		time = new Choice();
 		time.setBounds(149, 163, 214, 38);
 		time.setFont(new Font("맑은 고딕", 0, 24));
@@ -64,25 +64,25 @@ public class Reservation extends WindowAdapter implements ActionListener {
 		time.add("19:00");
 		time.add("20:30");
 
-		RPeople = new JLabel("인원(명)");
-		RPeople.setBounds(44, 230, 87, 29);
-		RPeople.setFont(new Font("맑은 고딕", 0, 24));
-		RtPe = new JTextField("");
-		RtPe.setBounds(149, 228, 214, 38);
-		RtPe.setFont(new Font("맑은 고딕", 0, 24));
-		PM = new JLabel("※ 6명 이상 단체 예약 시 전화 먼저 부탁드립니다.");
-		PM.setBounds(155, 266, 300, 16);
-		PM.setForeground(Color.red);
+		lPeople = new JLabel("인원(명)");
+		lPeople.setBounds(44, 230, 87, 29);
+		lPeople.setFont(new Font("맑은 고딕", 0, 24));
+		fPe = new JTextField("");
+		fPe.setBounds(149, 228, 214, 38);
+		fPe.setFont(new Font("맑은 고딕", 0, 24));
+		lPM = new JLabel("※ 6명 이상 단체 예약 시 전화 먼저 부탁드립니다.");
+		lPM.setBounds(155, 266, 300, 16);
+		lPM.setForeground(Color.red);
 
-		Rmemo = new JLabel("요청사항");
-		Rmemo.setBounds(42, 294, 96, 29);
-		Rmemo.setFont(new Font("맑은 고딕", 0, 24));
-		Rtme = new JTextField("");
-		Rtme.setBounds(149, 293, 214, 38);
-		Rtme.setFont(new Font("맑은 고딕", 0, 24));
-		MM = new JLabel("※ 요청사항이 있으시면 기재해주세요.");
-		MM.setBounds(155, 325, 215, 29);
-		MM.setForeground(Color.red);
+		lmemo = new JLabel("요청사항");
+		lmemo.setBounds(42, 294, 96, 29);
+		lmemo.setFont(new Font("맑은 고딕", 0, 24));
+		fme = new JTextField("");
+		fme.setBounds(149, 293, 214, 38);
+		fme.setFont(new Font("맑은 고딕", 0, 24));
+		lMM = new JLabel("※ 요청사항이 있으시면 기재해주세요.");
+		lMM.setBounds(155, 325, 215, 29);
+		lMM.setForeground(Color.red);
 
 		res = new JButton("예약하기");
 		res.setBounds(151, 397, 148, 59);
@@ -90,22 +90,21 @@ public class Reservation extends WindowAdapter implements ActionListener {
 		res.setContentAreaFilled(false);
 		res.addActionListener(this);
 
-		Rsn.add(RId);
-		Rsn.add(RtId);
-		Rsn.add(RDate);
-		Rsn.add(time);
-		Rsn.add(datePicker);
-		Rsn.add(RTime);
+		rsn.add(lId);
+		rsn.add(fId);
+		rsn.add(lDate);
+		rsn.add(time);
+		rsn.add(datePicker);
+		rsn.add(lTime);
+		rsn.add(lPeople);
+		rsn.add(fPe);
+		rsn.add(lPM);
+		rsn.add(lmemo);
+		rsn.add(fme);
+		rsn.add(lMM);
+		rsn.add(res);
 
-		Rsn.add(RPeople);
-		Rsn.add(RtPe);
-		Rsn.add(PM);
-		Rsn.add(Rmemo);
-		Rsn.add(Rtme);
-		Rsn.add(MM);
-		Rsn.add(res);
-
-		Rsn.setVisible(true);
+		rsn.setVisible(true);
 	}
 
 	@Override
@@ -117,14 +116,14 @@ public class Reservation extends WindowAdapter implements ActionListener {
 
 		String GDay = "" + Y + "-" + M + "-" + D;
 		String GTime = time.getSelectedItem();
-		String GPeople = RtPe.getText();
-		String GMemo = Rtme.getText();
+		String GPeople = fPe.getText();
+		String GMemo = fme.getText();
 
 		System.out.println(GDay);
 
 		if (e.getActionCommand().equals("예약하기")) {
 			dao.Reslist(ID, GDay, GTime, GPeople, GMemo);
-			Rsn.dispose();
+			rsn.dispose();
 		}
 	}
 }
