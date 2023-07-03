29a2c7b66cb25692402ccb4b96a38476b4707029
Class01 <Class01@DESKTOP-ILL1MFM>
2023-07-03 14:46:25 +0900

    ReRes

--- Portfolio_num1/src/Main/ReRes.java
+++ Portfolio_num1/src/Main/ReRes.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class ReRes extends WindowAdapter implements ActionListener {
 	private JFrame re_Res;
-	private JButton Change, Cancle;
+	private JButton change, cancle;
 	private String ID;
 
 	public void windowClosing(WindowEvent e) {
@@ -22,20 +22,20 @@ public class ReRes extends WindowAdapter implements ActionListener {
 		re_Res.setLocationRelativeTo(null);
 		re_Res.getContentPane().setBackground(Color.white);
 
-		Change = new JButton("예약 변경");
-		Change.setBounds(100, 91, 155, 55);
-		Change.setContentAreaFilled(false);
-		Change.setFont(new Font("맑은 고딕", 0, 24));
-		Change.addActionListener(this);
+		change = new JButton("예약 변경");
+		change.setBounds(100, 91, 155, 55);
+		change.setContentAreaFilled(false);
+		change.setFont(new Font("맑은 고딕", 0, 24));
+		change.addActionListener(this);
 
-		Cancle = new JButton("예약 취소");
-		Cancle.setBounds(296, 91, 155, 55);
-		Cancle.setContentAreaFilled(false);
-		Cancle.setFont(new Font("맑은 고딕", 0, 24));
-		Cancle.addActionListener(this);
+		cancle = new JButton("예약 취소");
+		cancle.setBounds(296, 91, 155, 55);
+		cancle.setContentAreaFilled(false);
+		cancle.setFont(new Font("맑은 고딕", 0, 24));
+		cancle.addActionListener(this);
 
-		re_Res.add(Change);
-		re_Res.add(Cancle);
+		re_Res.add(change);
+		re_Res.add(cancle);
 
 		re_Res.setVisible(true);
 		re_Res.addWindowListener(this);
