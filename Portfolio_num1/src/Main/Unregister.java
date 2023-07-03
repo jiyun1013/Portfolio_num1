db91acc603d9ad4c23db516b03619e6108480cce
Class01 <Class01@DESKTOP-ILL1MFM>
2023-07-03 14:47:47 +0900

    Unregister

--- Portfolio_num1/src/Main/Unregister.java
+++ Portfolio_num1/src/Main/Unregister.java
@@ -6,22 +6,22 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Unregister extends WindowAdapter implements ActionListener {
-	private JFrame Unreg;
+	private JFrame unreg;
 	private String ID;
 	private JLabel mainText, subText;
-	private JButton No, Ok;
+	private JButton no, ok;
 	private MemberDAO dao;
 
 	public Unregister(String ID) {
 		this.ID = ID;
 		dao = new MemberDAO();
 
-		Unreg = new JFrame("탈퇴하기");
-		Unreg.setSize(500, 300);
-		Unreg.setLayout(null);
-		Unreg.setResizable(false);
-		Unreg.setLocationRelativeTo(null);
-		Unreg.getContentPane().setBackground(Color.white);
+		unreg = new JFrame("탈퇴하기");
+		unreg.setSize(500, 300);
+		unreg.setLayout(null);
+		unreg.setResizable(false);
+		unreg.setLocationRelativeTo(null);
+		unreg.getContentPane().setBackground(Color.white);
 
 		mainText = new JLabel("<html><center>사용하고 계신 아이디는 탈퇴할 경우<br>재사용 및 복구가 불가능합니다.</center></html>");
 		mainText.setBounds(70, 44, 450, 58);
@@ -30,38 +30,38 @@ public class Unregister extends WindowAdapter implements ActionListener {
 		subText.setBounds(145, 113, 230, 20);
 		subText.setForeground(Color.red);
 
-		No = new JButton("취소");
-		No.setBounds(70, 160, 150, 50);
-		No.setFont(new Font("맑은 고딕", 0, 20));
-		No.setContentAreaFilled(false);
-		No.addActionListener(this);
+		no = new JButton("취소");
+		no.setBounds(70, 160, 150, 50);
+		no.setFont(new Font("맑은 고딕", 0, 20));
+		no.setContentAreaFilled(false);
+		no.addActionListener(this);
 
-		Ok = new JButton("탈퇴");
-		Ok.setBounds(280, 160, 150, 50);
-		Ok.setFont(new Font("맑은 고딕", 0, 20));
-		Ok.setContentAreaFilled(false);
-		Ok.addActionListener(this);
+		ok = new JButton("탈퇴");
+		ok.setBounds(280, 160, 150, 50);
+		ok.setFont(new Font("맑은 고딕", 0, 20));
+		ok.setContentAreaFilled(false);
+		ok.addActionListener(this);
 
-		Unreg.add(mainText);
-		Unreg.add(subText);
-		Unreg.add(No);
-		Unreg.add(Ok);
+		unreg.add(mainText);
+		unreg.add(subText);
+		unreg.add(no);
+		unreg.add(ok);
 
-		Unreg.setVisible(true);
+		unreg.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if (e.getActionCommand().equals("취소")) {
-			Unreg.dispose();
+			unreg.dispose();
 		}
 
 		if (e.getActionCommand().equals("탈퇴")) {
 			System.out.println(ID);
 			dao.Unrelist(ID);
 			JOptionPane.showMessageDialog(null, "탈퇴가 완료되었습니다.");
-			Unreg.dispose();
+			unreg.dispose();
 		}
 	}
 }
