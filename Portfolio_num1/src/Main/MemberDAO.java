4f8bb3f448a63bbe9b53b88cf98d642cd89f03d4
Class01 <Class01@DESKTOP-ILL1MFM>
2023-07-06 12:00:30 +0900

    MemberDAO

--- Portfolio_num1/src/Main/MemberDAO.java
+++ Portfolio_num1/src/Main/MemberDAO.java
@@ -3,7 +3,7 @@ package Main;
 import java.sql.*;
 import java.util.*;
 
- //회원 정보 관리 멤버다오
+//회원 정보 관리 멤버다오
 public class MemberDAO {
 	String driver = "oracle.jdbc.driver.OracleDriver";
 	String url = "jdbc:oracle:thin:@localhost:1521/xe";
@@ -13,6 +13,41 @@ public class MemberDAO {
 	private Connection con;
 	private Statement stmt;
 	private ResultSet rs;
+
+	// 회원가입-아이디 중복 체크
+	public ArrayList<MemberVo> Check(String id) {
+		ArrayList<MemberVo> check = new ArrayList<MemberVo>();
+		try {
+			connDB();
+
+			String query = "SELECT * FROM MEMBER";
+			if (id != null) {
+				query += " where CMS_ID =TRIM('" + id + "')";
+			}
+			System.out.println("SQL : " + query);
+
+			rs = stmt.executeQuery(query);
+			rs.last();
+			System.out.println("rs.getRow() : " + rs.getRow());
+
+			if (rs.getRow() == 0) {
+				System.out.println("0 row selected...");
+			} else {
+				System.out.println(rs.getRow() + "row selected...");
+				rs.previous();
+				while (rs.next()) {
+					String ID = rs.getString("CMS_ID");
+					System.out.println(ID);
+
+					MemberVo data = new MemberVo(ID);
+					check.add(data);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return check;
+	}
 
 	// 회원가입
 	public void Newlist(String NewId, String NewPwd, String NewNm, String NewBt, String NewEm) {
@@ -120,31 +155,39 @@ public class MemberDAO {
 	}
 
 	// 비밀번호 찾기-조회
-	public void Pwdlist(String FindId, String FindEm) {
+	public ArrayList<MemberVo> Pwdlist(String FindId, String FindEm) {
+		ArrayList<MemberVo> Id = new ArrayList<MemberVo>();
 		try {
-			Class.forName(driver);
-			System.out.println("jdbc driver loading success.");
-			Connection conn = DriverManager.getConnection(url, user, password);
-			System.out.println("oracle connection success.\n");
-			Statement stmt = conn.createStatement();
-
-			String Fquery  = "SELECT CMS_ID FROM MEMBER WHERE CMS_ID = '" + FindId + "' AND EMAIL = '" + FindEm + "'";
+			connDB();
+			if (FindId != null && FindEm != null) {
+				String Fquery = "SELECT CMS_ID FROM MEMBER WHERE CMS_ID = '" + FindId + "' AND EMAIL = '" + FindEm
+						+ "'";
 				System.out.println("SQL : " + Fquery);
 
-				boolean b = stmt.execute(Fquery);
-				if(!b) {
-					System.out.println("Find user");
-				}else {
-					System.out.println("Can not find user");
+				rs = stmt.executeQuery(Fquery);
+				rs.last(); // 마지막으로 가는 것
+				System.out.println("rs.getRow() : " + rs.getRow());
+
+				if (rs.getRow() == 0) {
+					System.out.println("0 row selected.....");
+				} else {
+					System.out.println(rs.getRow() + "row selected.....");
+					rs.previous(); // 직전 위치로 가는 것
+					while (rs.next()) {
+						String CI = rs.getString("CMS_ID");
+
+						MemberVo NewCI = new MemberVo(CI);
+						Id.add(NewCI);
+					}
 				}
-			
-		} catch (ClassNotFoundException e) {
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (SQLException e) {
-			System.out.println(e);
 		}
+		return Id;
 	}
-	//비밀번호 찾기-변경
+
+	// 비밀번호 찾기-변경
 	public void NewPwd(String ID, String NPwd) {
 		try {
 			Class.forName(driver);
@@ -153,16 +196,16 @@ public class MemberDAO {
 			System.out.println("oracle connection success.\n");
 			Statement stmt = conn.createStatement();
 
-			String Nquery  = "UPDATE MEMBER SET CMS_PWD = '" + NPwd + "' WHERE CMS_ID ='" + ID + "'";
-				System.out.println("SQL : " + Nquery);
+			String Nquery = "UPDATE MEMBER SET CMS_PWD = '" + NPwd + "' WHERE CMS_ID ='" + ID + "'";
+			System.out.println("SQL : " + Nquery);
 
-				boolean b = stmt.execute(Nquery);
-				if(!b) {
-					System.out.println("Change password");
-				}else {
-					System.out.println("Not");
-				}
-			
+			boolean b = stmt.execute(Nquery);
+			if (!b) {
+				System.out.println("Change password");
+			} else {
+				System.out.println("Not");
+			}
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
@@ -201,8 +244,10 @@ public class MemberDAO {
 
 	// 비밀번호 변경 : 새 비밀번호 설정하기
 	public void ReNewPwd(String id, String pwd) {
-/*	      RePwd RI = new RePwd(id);  잘못 생각한 부분
-	      RePwd RP = new RePwd(pwd); 이거 넣으면 창 하나씩 더 생김*/
+		/*
+		 * RePwd RI = new RePwd(id); 잘못 생각한 부분 RePwd RP = new RePwd(pwd); 이거 넣으면 창 하나씩 더
+		 * 생김
+		 */
 
 		try {
 			Class.forName(driver);
@@ -227,7 +272,7 @@ public class MemberDAO {
 			System.out.println(e);
 		}
 	}
-	
+
 	// 탈퇴하기
 	public void Unrelist(String ID) {
 		ArrayList<MemberVo> Unrelist = new ArrayList<MemberVo>();
@@ -257,6 +302,34 @@ public class MemberDAO {
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
+	}
+	
+	//마이페이지 이름 출력
+	public String Name(String ID) {
+		String name = "";
+		try {
+			connDB();
+
+			String query = "SELECT NAME FROM MEMBER WHERE CMS_ID = '" + ID + "'";
+			System.out.println("SQL : " + query);
+
+			rs = stmt.executeQuery(query);
+			rs.last();
+			System.out.println("rs.getRow() : " + rs.getRow());
+
+			if (rs.getRow() == 0) {
+				System.out.println("0 row selected...");
+			} else {
+				System.out.println(rs.getRow() + "row selected...");
+				rs.previous();
+				while (rs.next()) {
+					name = rs.getString("NAME");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return name;
 	}
 
 	private void connDB() {
